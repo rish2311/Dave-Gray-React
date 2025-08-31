814fe7aaa683ebc736d1453f33caf80f42ac2e29
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-08-31 18:49:42 +0530

    click events

--- 01tut/src/Content.jsx
+++ 01tut/src/Content.jsx
@@ -7,11 +7,27 @@ const Content = () => {
     return names[int];
   }
 
+  const handleClick = () => {
+    console.log("You clicked");
+  }
+
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  }
+
+  const handleClick3 = (e) => {
+    console.log(e.target.name);
+  }
+
   return (
     <main>
       <p>
         Hello {handlenameChange()}
       </p>
+
+      <button onClick={handleClick}>Click It</button>
+      <button onClick={() => handleClick2('Dave')}>Click Name</button>
+      <button onClick={(e) => handleClick3(e)}>Click Name 3</button>
 
     </main>
   )
